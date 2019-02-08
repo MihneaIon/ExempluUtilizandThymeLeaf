60f84394ff60c11e9f8adcbab12a6c1cb8d3887e
DESKTOP-ADEBHKV\Mihnea-PC <alexmihnea08@yahoo.com>
2019-02-08 18:53:46 +0200

    sdfsd

--- src/main/java/com/facultae/aos/temaFinala/userManagement/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.facultae.aos.temaFinala.userManagement.repository;
-
-import com.facultae.aos.temaFinala.userManagement.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Integer> {
-}
